8d2ab1d173431af651adea17cfe5a8490bb0b3a6
nithsP09 <nithyaparvathy09@gmail.com>
2025-11-02 16:15:48 +0530

    Add CSP and Permissions Policy headers

--- next.config.mjs
+++ next.config.mjs
@@ -11,22 +11,28 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
+          // Security Headers
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; img-src * data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+              "default-src 'self'; img-src * data:; script-src 'self'; style-src 'self' 'unsafe-inline'",
           },
           {
             key: "Permissions-Policy",
             value: "geolocation=(), microphone=(), camera=()",
           },
+
+          // Cross-Origin Protections
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
         ],
       },
     ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
